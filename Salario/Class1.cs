8695a4afed5ccf1c85a658a16603f78c259c1a80
Kiberio <dhiroshi3@gmail.com>
2021-10-20 14:47:00 -0300

    Comentarios

---
+++ Salario/Class1.cs
@@ -0,0 +1,29 @@
+﻿//Daniel Hiroshi Fugikawa - 200209
+namespace Salario
+{
+    class funcionarios
+    {
+        //Declaração das variaveis da classe
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+        public int Id { get; set; }
+
+        //Declaração do construtor
+        public funcionarios(string nome, double salario, int id)
+        {
+            Nome = nome;
+            Salario = salario;
+            Id = id;
+        }
+        //Forma de mostar os funcionários em lista
+        public override string ToString()
+        {
+            return Id + " , " + Nome + " , " + Salario;
+        }
+        //Função para aumentar o salario
+        public void AumentarSalario(double porcentagem)
+        {
+            Salario = Salario * porcentagem / 100;
+        }
+    }
+}
--- Salario/Program.cs
+++ Salario/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Daniel Hiroshi Fugikawa - 200209
+using System;
 
 namespace Salario
 {
@@ -10,6 +7,55 @@ namespace Salario
     {
         static void Main(string[] args)
         {
+            //Declaração do vetor
+            funcionarios[] vect = new funcionarios[100];
+            int[] vect2 = new int[1];
+            //Solicitando o numero de Funcionarios
+            Console.WriteLine("Digite o numero de Funcionarios a ser registrado: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                Console.WriteLine(" ");
+                //Informações dos funcionarios
+                Console.WriteLine($"Funcionario#{i}");
+                Console.WriteLine("Id:");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Nome:");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Salario:");
+                double salario = double.Parse(Console.ReadLine());
+
+                vect[id] = new funcionarios(nome, salario, id);
+            }
+            Console.WriteLine("");
+            //Informar o funcionário que ira receber o aumento
+            Console.WriteLine("Id do funcionário receberá aumento: ");
+            int id2 = int.Parse(Console.ReadLine());
+
+            if (vect[id2] != null)
+            {
+                //Conta para realizar o aumento
+                Console.WriteLine("Porcentagem de aumento de salario: ");
+                double porcentagem = double.Parse(Console.ReadLine());
+                porcentagem = porcentagem + 100;
+                vect[id2].AumentarSalario(porcentagem);
+            }
+            else
+            {
+                //Caso seja digitado um id inexistente
+                Console.WriteLine("Este funcionario não existe!");
+            }
+            //Atualização a lista dos funcinários com o aumento realizado
+            Console.WriteLine("Lista atualizada dos funcionarios da Bravo: ");
+            for (int i = 0; i < x + 1; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(vect[i]);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
